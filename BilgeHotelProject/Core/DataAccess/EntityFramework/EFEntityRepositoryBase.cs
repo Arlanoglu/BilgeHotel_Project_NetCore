946a6b1241028adc02f596788b88865bae16fbb9
Mustafa <arlanoglumustafa@hotmail.com>
2022-02-11 19:31:02 +0300

    EntityRepository oluşturuldu

---
+++ BilgeHotelProject/Core/DataAccess/EntityFramework/EFEntityRepositoryBase.cs
@@ -0,0 +1,49 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EFEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity> where TEntity : BaseEntity where TContext : DbContext
+    {
+        public bool Any(Expression<Func<TEntity, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Create(TEntity model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetDefault(Expression<Func<TEntity, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetList(TEntity model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(TEntity model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BilgeHotelProject/Core/DataAccess/IEntityRepository.cs
+++ BilgeHotelProject/Core/DataAccess/IEntityRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,13 @@ namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T : BaseEntity
     {
-        string Create { get; set; }
-        List<T> GetList { get; set; }
+        string Create(T model);
+        List<T> GetList(T model);
+        T GetById(int id);
+        string Update(T model);
+        string Delete(int id);
+        bool Any(Expression<Func<T, bool>> exp);
+        List<T> GetDefault(Expression<Func<T, bool>> exp);
+        
     }
 }
