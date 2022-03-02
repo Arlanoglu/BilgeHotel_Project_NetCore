5f7715d3e5f208201259d012f4d29ee6910a8083
Mustafa <arlanoglumustafa@hotmail.com>
2022-03-02 05:03:46 +0300

    navbar tamamlandı.

--- BilgeHotelProject/WebUI/Controllers/HomeController.cs
+++ BilgeHotelProject/WebUI/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WebUI.Models;
 
 namespace WebUI.Controllers
 {
@@ -20,18 +19,8 @@ namespace WebUI.Controllers
 
         public IActionResult Index()
         {
+            
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
