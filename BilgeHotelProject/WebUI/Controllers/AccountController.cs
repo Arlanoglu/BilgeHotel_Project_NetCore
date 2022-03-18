7c927a583c6312cfe7d4c21fcbb68bd67a520c8a
Mustafa <arlanoglumustafa@hotmail.com>
2022-03-18 04:46:10 +0300

    Login ve Register Tasarımları tamamlandı. user manager ve authentication işlemleri yapılacak.

---
+++ BilgeHotelProject/WebUI/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult Register()
+        //{
+        //    return View();
+        //}
+    }
+}
