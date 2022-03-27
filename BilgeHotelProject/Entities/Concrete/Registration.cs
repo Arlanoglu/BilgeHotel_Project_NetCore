2fe77a08c706bcf5647fceb5d8575787493927a9
Mustafa <arlanoglumustafa@hotmail.com>
2022-03-27 17:40:57 +0300

    Kayıt işlemleri için gerekli olan kayıtdurumu ve kayıttipi enjumları tanımlandı ve registration entitysi revize edildi.

--- BilgeHotelProject/Entities/Concrete/Registration.cs
+++ BilgeHotelProject/Entities/Concrete/Registration.cs
@@ -1,18 +1,25 @@
 ﻿using Core.Entities;
+using Entities.Enum;
 using System;
 
 namespace Entities.Concrete
 {
     public class Registration : BaseEntity
     {
+        public Registration()
+        {
+            RegistrationStatus = RegistrationStatus.GirisYapildi;
+        }
         public DateTime CheckInDate { get; set; }
         public DateTime CheckOutDate { get; set; }
         public int NumberOfPeople { get; set; }
         public decimal Price { get; set; }
-        public string RegistrationType { get; set; }
+        //public string RegistrationType { get; set; }
         public string Description { get; set; }
         public TimeSpan CheckInTime { get; set; }
         public TimeSpan CheckOutTime { get; set; }
+        public RegistrationType RegistrationType { get; set; }
+        public RegistrationStatus RegistrationStatus { get; set; }
         //Todo: Giriş saati farketmeksizin 14:00 olacaktır. Map işleminde yazıldı kontrol edilecek.
         public int? ReservationID { get; set; }
 
