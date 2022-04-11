f5f76b081fc940234851afdc87fb7b21e5499513
Mustafa <arlanoglumustafa@hotmail.com>
2022-04-11 03:52:36 +0300

    iletişim sayfası mesaj gönderimi tamamlandı.

--- BilgeHotelProject/WebUI/Controllers/ContactController.cs
+++ BilgeHotelProject/WebUI/Controllers/ContactController.cs
@@ -10,6 +10,8 @@ using WebUI.Models.Contact;
 using Core.Utilities.Results.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
+using Common;
+using Core.Utilities.Results.Abstract;
 
 namespace WebUI.Controllers
 {
@@ -20,14 +22,18 @@ namespace WebUI.Controllers
         private readonly IMessageService messageService;
         private readonly SignInManager<AppUser> signInManager;
         private readonly UserManager<AppUser> userManager;
+        private readonly ISettingService settingService;
+        private readonly IResult result;
 
-        public ContactController(IMapper mapper, IContactService contactService, IMessageService messageService, SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        public ContactController(IMapper mapper, IContactService contactService, IMessageService messageService, SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, ISettingService settingService, IResult result)
         {
             this.mapper = mapper;
             this.contactService = contactService;
             this.messageService = messageService;
             this.signInManager = signInManager;
             this.userManager = userManager;
+            this.settingService = settingService;
+            this.result = result;
         }
         public async Task<IActionResult> Index()
         {
@@ -38,24 +44,48 @@ namespace WebUI.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(VMMessage vMMessage)
+        public async Task<IActionResult> Index(VMMessage vMMessage)
         {
             if (ModelState.IsValid)
             {
-                if (signInManager.IsSignedIn(User))
+                var message = $"İsim Soyisim : {vMMessage.FirstName} {vMMessage.LastName}\n" +
+                              $"Eposta : {vMMessage.Email} \n" +
+                              $"Telefon No : {vMMessage.PhoneNumber}\n" +
+                              $"Konu : {vMMessage.Subject}\n\n" +
+                              $"Mesaj : \n\n {vMMessage.MessageContent}";
+
+                var setting = (await settingService.GetActive()).FirstOrDefault();
+                var mailResult = MailSender.SendMail("arlanoglumustafa@hotmail.com", "Müşteri İletişim", message, setting); //Todo: gönderilen mesajların karşılanacağı mail adresi değiştirilecek.
+
+                if (mailResult ==1)
                 {
-                    vMMessage.AppUserID = userManager.GetUserId(User);
-                }
-                var message = mapper.Map<Message>(vMMessage);
-                var messageResult = messageService.Create(message);
-                if (messageResult.ResultStatus==ResultStatus.Success)
-                {
-                    ViewBag.MessageSuccess = "Mesajınız başarıyla iletildi.";                    
+                    result.ResultStatus = ResultStatus.Success;
+                    result.Message = "Mesajınız başarıyla iletildi. En kısa zamanda tarafınıza dönüş yapılacaktır.";
+                    ViewBag.MessageResult = result;
                 }
                 else
                 {
-                    ModelState.AddModelError("SendMessageError", "Mesaj gönderiminde hata oluştu.");
-                }                
+                    result.ResultStatus = ResultStatus.Error;
+                    result.Message = "Mesaj gönderiminde hata oluştu.";
+                    ViewBag.MessageResult = result;
+                }
+
+                #region OldCode
+                //if (signInManager.IsSignedIn(User))
+                //{
+                //    vMMessage.AppUserID = userManager.GetUserId(User);
+                //}
+                //var message = mapper.Map<Message>(vMMessage);
+                //var messageResult = messageService.Create(message);
+                //if (messageResult.ResultStatus==ResultStatus.Success)
+                //{
+                //    ViewBag.MessageSuccess = "Mesajınız başarıyla iletildi.";                    
+                //}
+                //else
+                //{
+                //    ModelState.AddModelError("SendMessageError", "Mesaj gönderiminde hata oluştu.");
+                //}  
+                #endregion
             }
             var vmContact = JsonConvert.DeserializeObject<VMContact>(TempData["vmContact"].ToString());
             TempData.Keep("vmContact");
